8176dedf7db56e2165ac7909aadf131a63bb20ce
wscrd <wesscardua@gmail.com>
2014-02-25 15:06:10 -0300

    teste

--- Navigation/Navigation/LetraBViewController.m
+++ Navigation/Navigation/LetraBViewController.m
@@ -2,7 +2,7 @@
 //  LetraBViewController.m
 //  Navigation
 //
-//  Created by Vinicius Miana on 2/23/14.
+//  Created by Vinicius Miana on 23/2/14.
 //  Copyright (c) 2014 Vinicius Miana. All rights reserved.
 //
 
